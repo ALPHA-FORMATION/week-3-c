581bdbc884992b55eda40dbd8e21736f8d80fb79
ALPHA-FORMATION <jotsnapramanik@gmail.com>
2025-09-01 00:09:39 +0530

    Add files via upload

---
+++ Reverse linked list.c
@@ -0,0 +1,60 @@
+//Reverse linked list
+//https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* reverseList(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* curr = head;
+    struct Node* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;   
+        curr->next = prev;   
+        prev = curr;         
+        curr = next;         
+    }
+    return prev; 
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+int main() {
+    struct Node* head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(3);
+    head->next->next->next = newNode(4);
+
+    printf("Original List: ");
+    printList(head);
+
+    head = reverseList(head);
+
+    printf("Reversed List: ");
+    printList(head);
+
+    return 0;
+}
+
